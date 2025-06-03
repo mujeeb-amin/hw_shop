115823917415d31a5eb830c595073e26ac99ed1b
Mujeeb Rahman Amin <mujeebrahmanamin2@gmail.com>
2025-06-03 20:59:26 +0430

    CUD

--- products/admin.py
+++ products/admin.py
@@ -4,6 +4,14 @@ from django.contrib import admin
 
 from products.models import category, product
 
+class productAdmin(admin.ModelAdmin):
+    model = product
+    list_display = ["title", "price", "catagory", "updated_date","is_published"]
+    list_display_links = ["title", "price"]
+    list_editable = ["is_published"]
+    list_filter = ["catagory"]
+    # list_per_page = 2
+
 
 admin.site.register(category)
-admin.site.register(product)
+admin.site.register(product, productAdmin)
--- products/models.py
+++ products/models.py
@@ -2,18 +2,43 @@ from django.db import models
 
 # Create your models here.
 
+
 class category(models.Model):
     name = models.CharField(max_length=250)
 
     def __str__(self):
         return self.name
-    
+
+    # for changeing in admin panal
+    class Meta:
+        verbose_name = "category"
+        verbose_name_plural = "categories"
+
 
 class product(models.Model):
-    title = models.CharField(max_length=120,)
+    title = models.CharField(
+        max_length=120,
+    )
     price = models.FloatField(default=10)
     catagory = models.ForeignKey(category, on_delete=models.CASCADE)
-
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    type = models.CharField(
+        choices=[
+            ("for_kids", "For Kids"),
+            ("for_men", "For Men"),
+            ("for_women", "For women"),
+        ],
+        max_length=121,
+        blank=True,
+        null=True,
+    )
+    is_published = models.BooleanField()
 
     def __str__(self):
         return self.title
+
+    # for changing in database
+    class Meta:
+        # abstract = True
+        db_table = "products"
